aeb113f95cdf15edaa05a974ed7e2b4f8d400639
WeeDLY <jorgen.sater@gmail.com>
2018-10-02 12:58:31 +0200

    Added crash on startup

--- app/src/main/java/no/hiof/internote/internote/MainActivity.java
+++ app/src/main/java/no/hiof/internote/internote/MainActivity.java
@@ -1,13 +1,33 @@
 package no.hiof.internote.internote;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+    private FirebaseDatabase firebaseDatabase;
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener firebaseAuthStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference usersReference = firebaseDatabase.getReference("users");
+
+
     }
 }
