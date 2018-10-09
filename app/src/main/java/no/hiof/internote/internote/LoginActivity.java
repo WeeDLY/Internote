c5a65fc3d7e7df5fa86661a21aef8783ea96ed32
WeeDLY <jorgen.sater@gmail.com>
2018-10-09 11:43:46 +0200

    Can't open new activity

--- app/src/main/java/no/hiof/internote/internote/LoginActivity.java
+++ app/src/main/java/no/hiof/internote/internote/LoginActivity.java
@@ -1,7 +1,10 @@
 package no.hiof.internote.internote;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void BtnMain(View view) {
+        //Toast.makeText(view.getContext(), "asdasd", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(view.getContext(), MainActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/no/hiof/internote/internote/MainActivity.java
+++ app/src/main/java/no/hiof/internote/internote/MainActivity.java
@@ -1,5 +1,6 @@
 package no.hiof.internote.internote;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -7,18 +8,9 @@ import android.util.Log;
 import android.widget.Toast;
 
 
-import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseDatabase firebaseDatabase;
@@ -31,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+/*
         try{
             DatabaseReference myRef1 = FirebaseDatabase.getInstance().getReference(); //Getting root reference
             DatabaseReference myRef = myRef1.child("message"); //Write your child reference if any
@@ -42,24 +34,12 @@ public class MainActivity extends AppCompatActivity {
             //mDatabase.push().setValue(1);
             //DatabaseReference myRef = firebaseDatabase.getReference("users");
             usersReference = firebaseDatabase.getReference("users");
-            generateTestData();*/
+            generateTestData();
         }
         catch(Exception e){
             Log.d("EXCEPTIONDECEPTION", "onCreate: " + e.getMessage());
-        }
+        }*/
         //generateTestData();
-
-    }
-    private void generateTestData(){
-        ArrayList<String> data = new ArrayList<String>();
-        data.add("a");
-        data.add("b");
-        data.add("c");
-
-        for(String s : data){
-            Log.d("generateTest", "Pushing: " + s);
-            usersReference.push().setValue(s);
-        }
 
     }
 
