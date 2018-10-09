1e5c306f9114ba7f0ef02f1f76ef9125c68d9c64
WeeDLY <jorgen.sater@gmail.com>
2018-10-09 15:59:49 +0200

    Added SEttins and NoteBasicImage model

--- app/src/main/java/no/hiof/internote/internote/model/NoteBasic.java
+++ app/src/main/java/no/hiof/internote/internote/model/NoteBasic.java
@@ -2,6 +2,9 @@ package no.hiof.internote.internote.model;
 
 import java.util.Date;
 
+/*
+    This is a basic template for Note, that only consists of text
+ */
 public class NoteBasic extends Note {
     public NoteBasic(){ super(); }
     public NoteBasic(Date creationDate){ super(creationDate); }
