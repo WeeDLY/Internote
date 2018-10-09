71d55068cf4f3ad76f04352eb9388fde913d64d4
WeeDLY <jorgen.sater@gmail.com>
2018-10-09 14:54:31 +0200

    Trying to get firebase-auth to work

--- app/src/main/java/no/hiof/internote/internote/LoginActivity.java
+++ app/src/main/java/no/hiof/internote/internote/LoginActivity.java
@@ -1,16 +1,132 @@
 package no.hiof.internote.internote;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ui.email.EmailActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserInfo;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Arrays;
 
 public class LoginActivity extends AppCompatActivity {
+    private static final int RC_SIGN_IN = 1;
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener firebaseAuthStateListener;
+    private FirebaseUser user;
+    private FirebaseDatabase firebaseDatabase;
+
+    private String email = "asd@gmail.com";
+    private String password = "asdasd";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        createAuthenticationListener();
+    }
+
+    public void GoToMainActivity(View view){
+        Intent intentMain = new Intent(view.getContext(), MainActivity.class);
+        startActivity(intentMain);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (firebaseAuth.getCurrentUser() != null){
+            Toast.makeText(this.getApplicationContext(), "User: " + user.getDisplayName(), Toast.LENGTH_LONG).show();
+            //GoToMainActivity();
+        }
+    }
+
+    private void createAuthenticationEmailPasswordListener() {
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            // Successfull sign-in
+                            user = firebaseAuth.getCurrentUser();
+                        }
+                        else{
+                            // Failed to sign-in
+                            //Toast.makeText(EmailActivity.this, "Authentication failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (firebaseAuthStateListener != null)
+            firebaseAuth.addAuthStateListener(firebaseAuthStateListener);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (firebaseAuthStateListener != null)
+            firebaseAuth.removeAuthStateListener(firebaseAuthStateListener);
+    }
+
+    /*
+        creates a authentication listener that prompts the user to log in
+     */
+    private void createAuthenticationListener() {
+        Log.i("tag", "CreateAuthentication listener");
+        firebaseAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(FirebaseAuth firebaseAuth) {
+                FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+                if (firebaseUser == null) {
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setIsSmartLockEnabled(false)
+                                    .setAvailableProviders(
+                                            Arrays.asList(new AuthUI.IdpConfig.EmailBuilder().build(),
+                                                    new AuthUI.IdpConfig.GoogleBuilder().build()))
+                                    .build(),
+                            RC_SIGN_IN);
+                }
+            }
+        };
+    }
+
+    /*
+        Event that is fired after createAuthenticationListener
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                Toast.makeText(this, "Signed in as " + user.getDisplayName(), Toast.LENGTH_SHORT).show();
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "Sign in canceled", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
     }
 
     public void BtnStartMain(View view) {
