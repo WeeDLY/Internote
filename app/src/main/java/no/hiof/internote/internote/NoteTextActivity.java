6477349c1c77d34b81b513f68044f5963f67f63a
Kake <jorgen.sater@gmail.com>
2018-10-16 13:25:09 +0200

    NoteTextActivity is fully functioning!

--- app/src/main/java/no/hiof/internote/internote/NoteTextActivity.java
+++ app/src/main/java/no/hiof/internote/internote/NoteTextActivity.java
@@ -24,48 +24,48 @@ import no.hiof.internote.internote.model.*;
 public class NoteTextActivity extends AppCompatActivity {
     private FirebaseUser user;
     private NoteDetailed noteDetailed;
+    private EditText textTitle;
+    private EditText textLastEdited;
     private EditText textContent;
 
     private DatabaseReference noteDetailedReference;
 
-    private String currentNoteDetailedKey = "";
-    private String currentNoteOverviewKey = "";
+    private String currentNoteDetailedKey;
+    private String currentNoteOverviewKey;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_text);
 
+        textTitle = findViewById(R.id.textTitle);
+        textLastEdited = findViewById(R.id.textLastEdited);
         textContent = findViewById(R.id.textContent);
 
         user = getIntent().getParcelableExtra(Settings.FIREBASEUSER_INTENT);
         currentNoteDetailedKey = getIntent().getStringExtra(Settings.INTENT_NOTEDETAILED_KEY);
         currentNoteOverviewKey = getIntent().getStringExtra(Settings.INTENT_NOTEOVERVIEW_KEY);
 
-        if(user == null){
-            noteDetailed = new NoteDetailed("New note", "content", System.currentTimeMillis());
-            FillFields();
-            return;
-        }
-
-        if (!currentNoteDetailedKey.equals("")){
+        if (currentNoteDetailedKey != null){
             retrieveDocument(currentNoteDetailedKey);
         }
+        else{
+            noteDetailed = new NoteDetailed("New note", "", System.currentTimeMillis());
+            FillFields();
+        }
+
     }
 
     private void FillFields(){
         textContent.setText(noteDetailed.getContent());
 
-        EditText textCreationDate = findViewById(R.id.textCreationDate);
-        textCreationDate.setText(new Date(noteDetailed.getCreationDate() * 1000).toString());
+        textLastEdited.setText(new Date(noteDetailed.getCreationDate() * 1000).toString());
 
         TextView textTitle = findViewById(R.id.textTitle);
         textTitle.setText(noteDetailed.getTitle());
     }
 
-    /*
-        Retrieves document information from firebase
-     */
+    // Retrieves document information from firebase
     private void retrieveDocument(String documentId){
         FirebaseDatabase databaseReference = FirebaseDatabase.getInstance();
         DatabaseReference documentReference = databaseReference.getReference();
@@ -107,6 +107,8 @@ public class NoteTextActivity extends AppCompatActivity {
             return;
         }
 
+        // Update current noteDetailed
+        noteDetailed.setTitle(textTitle.getText().toString());
         noteDetailed.setContent(textContent.getText().toString());
         noteDetailed.setLastEdited(System.currentTimeMillis());
 
@@ -114,31 +116,29 @@ public class NoteTextActivity extends AppCompatActivity {
         DatabaseReference userReference = FirebaseDatabase.getInstance().getReference().child(user.getUid());
         // Update NoteDetailed to firebase
 
-        // Note was loaded from a previous note. Overwrite with new data
-        if(!currentNoteDetailedKey.equals("")){
-            noteDetailedReference = userReference.child(Settings.FIREBASE_NOTE_DETAILED).child(currentNoteDetailedKey);
-        }
         // Note does not exist, writes a new NoteDetailed to database
-        else{
+        if(currentNoteDetailedKey == null){
             noteDetailedReference = userReference.child(Settings.FIREBASE_NOTE_DETAILED).push();
+        }
+        // Note was loaded from a previous note. Overwrite with new data
+        else{
+            noteDetailedReference = userReference.child(Settings.FIREBASE_NOTE_DETAILED).child(currentNoteDetailedKey);
         }
         noteDetailedReference.setValue(noteDetailed);
 
 
+        // NoteOver does not exist, writes a new NoteOverview to database
+        if(currentNoteOverviewKey == null){
+            DatabaseReference noteOverviewReference = userReference.child(Settings.FIREBASE_NOTE_OVERVIEW).push();
+            noteOverviewReference.setValue(new NoteOverview(noteDetailed, noteDetailedReference.getKey()));
+
+        }
         // Update NoteOverview to firebase
-        if(!currentNoteOverviewKey.equals("")){
+        else{
             DatabaseReference noteOverviewReference = userReference.child(Settings.FIREBASE_NOTE_OVERVIEW).child(currentNoteOverviewKey);
             noteOverviewReference.child("lastEdited").setValue(noteDetailed.getLastEdited());
             noteOverviewReference.child("title").setValue(noteDetailed.getTitle());
-
         }
-        // NoteOver does not exist, writes a new NoteOverview to database
-        else{
-            DatabaseReference noteOverviewReference = userReference.child(Settings.FIREBASE_NOTE_OVERVIEW).push();
-            noteOverviewReference.setValue(new NoteOverview(noteDetailed, noteDetailedReference.getKey()));
-        }
-
-        Log.d("noteoverviewkey", "TTT: " + currentNoteOverviewKey);
 
         // Display that it was saved and auto-moves user to MainActivity
         Toast.makeText(view.getContext(), "Saved note: " + noteDetailedReference.getKey(), Toast.LENGTH_LONG).show();
