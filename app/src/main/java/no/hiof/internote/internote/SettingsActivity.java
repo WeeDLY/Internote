64b38b452f7a9f8a38f7359080d1580bdf60e478
mariuskarlsen <marius.r.karlsen@hotmail.com>
2018-11-07 13:37:44 +0100

    Working on savedpreferences in settingsActivity

--- app/src/main/java/no/hiof/internote/internote/SettingsActivity.java
+++ app/src/main/java/no/hiof/internote/internote/SettingsActivity.java
@@ -1,6 +1,7 @@
 package no.hiof.internote.internote;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -18,6 +19,11 @@ public class SettingsActivity extends AppCompatActivity {
     private List<String> colorThemeList = new ArrayList<>();
     private Spinner spinnerColorTheme;
 
+    public static final String SHARED_PREFS = "sharedPrefs";
+    public static final String SPINNER = "Spinner";
+
+    private int spinnerPosition;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(Settings.getTheme());
@@ -25,6 +31,7 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
 
         setUpSpinner();
+        loadData();
     }
 
     private void setUpSpinner(){
@@ -52,7 +59,22 @@ public class SettingsActivity extends AppCompatActivity {
     }
 
     public void applyChanges(View view){
+        saveData();
+
         Intent intentMain = new Intent(this, MainActivity.class);
         startActivity(intentMain);
+    }
+
+    public void saveData(){
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putInt(SPINNER, spinnerColorTheme.getSelectedItemPosition());
+    }
+
+    public void loadData(){
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+
+        spinnerPosition = sharedPreferences.getInt(SPINNER, 0);
     }
 }
